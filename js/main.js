c5558ebe8730b6b28a482f16b647d1ecc9ab2272
8810-falcon <ogu881129suki@gmail.com>
2023-01-11 01:14:33 +0900

    DBに参加者名を反映
    以下の仕組みで反映
    ・セッション内にユーザーリストを作成
    ・データの変更がある度にセッション内のユーザーリストをDBに反映させて最新情報を保存

--- js/main.js
+++ js/main.js
@@ -1,5 +1,41 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  set,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/9.8.2/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBFm9XqOojiZcViSu-rC7r1M8v7PF6qP0E",
+  authDomain: "nomikai-36234.firebaseapp.com",
+  databaseURL: "https://nomikai-36234-default-rtdb.firebaseio.com",
+  projectId: "nomikai-36234",
+  storageBucket: "nomikai-36234.appspot.com",
+  messagingSenderId: "1063040288133",
+  appId: "1:1063040288133:web:909e5a99a3c175694830ec",
+  measurementId: "G-NRV1BMMGEG",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+//グローバル変数クエリパラメータ
+let queryParam = location.search.replaceAll("?id=", "");
+
 (function ($) {
   $(document).ready(function () {
+    console.log(queryParam);
+    //クエリパラメータが存在しない場合、初期画面に飛ばす
+    if (queryParam.length == 0) {
+      window.location.href = "start.html";
+    }
     //デバイス判定
     if (!isSmartPhone()) {
       $("body").children().remove();
@@ -17,7 +53,13 @@
     }
 
     let nameList = window.sessionStorage.getItem(["nameList"]);
-    dispAddedMember();
+    dispMemeber();
+
+    //開発用にセッション削除
+    $("#pay-mgr").on("click", function () {
+      window.sessionStorage.clear();
+      console.log("セッション削除");
+    });
 
     //名前の入力&リスト追加
     $("#member-name-add").on("click", function () {
@@ -47,7 +89,9 @@
           [JSON.stringify(sessionNameList)]
         );
       }
-      dispAddedMember();
+      //dispAddedMember();
+      changeMemberData();
+      dispMemeber();
       //input欄の削除
       $("#member-name-input").val("");
     });
@@ -58,6 +102,8 @@
       const targetIdNum = targetId.replaceAll("delete-added-member-", "");
       const deleteName = $(this).prev().text();
       deleteFromSession(deleteName);
+      changeMemberData();
+      dispMemeber();
     });
     //人数の割り出し
     $("#make-group").on("click", function () {
@@ -128,26 +174,58 @@ function dispAddedMember() {
   }
 }
 
+async function dispMemeber() {
+  //既存リストを削除
+  $("#added-member-list").children().remove();
+  const nameList = await onValue(
+    ref(database, queryParam + "/member"),
+    (snapshot) => {
+      if (snapshot.val() == null) {
+        return;
+      }
+      const nameList = Object.values(snapshot.val()["memberList"]);
+      console.log("nameList:" + nameList);
+      $("#current-member-amount").text(nameList.length);
+      for (let i = 0; i < nameList.length; i++) {
+        const listElem = `
+            <li class="d-flex justify-content-between added-name-list">
+                <div class="added-name" id="added-name-${i}">${nameList[i]}</div>
+                <button type="button" class="delete-added-member" id="delete-added-member-${i}">削除</button>
+            </li>
+        `;
+        $("#added-member-list").append(listElem);
+      }
+    }
+  );
+}
+
 //追加したメンバーの削除&制御用メソッド
 function deleteFromSession(name) {
-  const sessionNameList = window.sessionStorage
-    .getItem(["nameList"])
+  const sessionNameList = window.sessionStorage.getItem(["nameList"]);
+  console.log("sessionNameList:" + sessionNameList.length);
+  const fixedNameList = sessionNameList
     .replaceAll('"', "")
     .replaceAll("[", "")
     .replaceAll("]", "")
-    .split(",");
-  const fixedNameList = sessionNameList.filter(function (item) {
-    return item !== name;
-  });
-  if (fixedNameList == "") {
+    .split(",")
+    .filter(function (item) {
+      return item !== name;
+    });
+  if (fixedNameList == null || fixedNameList == "") {
     window.sessionStorage.clear();
+    $("#current-member-amount").text("0");
+    console.log("残りメンバー0");
   } else {
-    window.sessionStorage.setItem(
-      ["nameList"],
-      [JSON.stringify(fixedNameList)]
-    );
+    console.log("残りメンバーが存在");
+    if (fixedNameList == "") {
+      window.sessionStorage.clear();
+    } else {
+      window.sessionStorage.setItem(
+        ["nameList"],
+        [JSON.stringify(fixedNameList)]
+      );
+    }
   }
-  $("#current-member-amount").text(fixedNameList.length);
 }
 
 //人数の割り出し制御
@@ -222,5 +300,28 @@ function isSmartPhone() {
     return true;
   } else {
     return false;
+  }
+}
+
+//DBに参加者名を追加
+async function changeMemberData() {
+  let sessionNameList = window.sessionStorage.getItem(["nameList"]);
+  console.log("sessionNameList:" + sessionNameList);
+  if (sessionNameList == null || sessionNameList == "") {
+    const nameList = [];
+    await set(ref(database, queryParam + "/member"), {
+      memberList: nameList,
+    });
+  } else {
+    const nameList = sessionNameList
+      .replaceAll(null, "")
+      .replaceAll('"', "")
+      .replaceAll("[", "")
+      .replaceAll("]", "")
+      .split(",");
+    console.log(nameList);
+    await set(ref(database, queryParam + "/member"), {
+      memberList: nameList,
+    });
   }
 }
